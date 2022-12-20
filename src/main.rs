8d75e360cf178a7d3dfb6dc5cceb1f57a95e59c0
manorajesh <mano@raje.sh>
2022-12-20 14:12:18 -0800

    Added basic image output and progress indicator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::{stderr, Write};
+
 fn main() {
-    println!("Hello, world!");
+    // Image
+    let image_width: i32 = 256;
+    let image_height: i32 = 256;
+
+    // Render
+    print!("P3\n{} {}\n255\n", image_width, image_height);
+    for j in (0..image_height).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
+        stderr().flush().unwrap();
+        for i in 0..image_width {
+            let r = i as f64 / (image_width-1) as f64;
+            let g = j as f64 / (image_height-1) as f64;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            print!("{} {} {}\n", ir, ig, ib);
+        }
+    }
+    eprint!("\nDone!\n");
 }
